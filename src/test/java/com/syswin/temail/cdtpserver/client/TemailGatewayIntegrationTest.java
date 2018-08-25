2395da073f2526ed80468653630dbd32dd2ae364
殷湘 <yinxiang@syswin.com>
2018-08-25 15:05:52 +0800

    integration test with rocket mq

--- src/test/java/com/syswin/temail/cdtpserver/client/TemailGatewayIntegrationTest.java
+++ src/test/java/com/syswin/temail/cdtpserver/client/TemailGatewayIntegrationTest.java
@@ -28,6 +28,7 @@ import com.syswin.temail.gateway.containers.RocketMqNameServerContainer;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import javax.annotation.Resource;
+import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.common.message.Message;
 import org.jetbrains.annotations.NotNull;
@@ -48,6 +49,7 @@ import org.testcontainers.containers.Network;
     "temail.verifyUrl=http://localhost:8090/verify",
     "temail.dispatchUrl=http://localhost:8090/dispatch",
     "temail.updateSocketStatusUrl=http://localhost:8090/updateStatus",
+    "temail.mqTopic=temail-gateway",
     "temail.allIdleTimeSeconds=3"
 })
 public class TemailGatewayIntegrationTest {
@@ -55,10 +57,11 @@ public class TemailGatewayIntegrationTest {
   private static final Gson GSON = new Gson();
 
   private static final Network NETWORK = Network.newNetwork();
+  private static final int MQ_SERVER_PORT = 9876;
   private static final RocketMqNameServerContainer rocketMqNameSrv = new RocketMqNameServerContainer()
       .withNetwork(NETWORK)
       .withNetworkAliases("namesrv")
-      .withFixedExposedPort(9876, 9876);
+      .withFixedExposedPort(MQ_SERVER_PORT, MQ_SERVER_PORT);
 
 
   private static final RocketMqBrokerContainer rocketMqBroker = new RocketMqBrokerContainer()
@@ -84,6 +87,7 @@ public class TemailGatewayIntegrationTest {
   private final BlockingQueue<CDTPPackage> receivedPackages = new LinkedBlockingQueue<>();
   private final BlockingQueue<CDTPPackage> toBeSentPackages = new LinkedBlockingQueue<>();
   private final EchoClient client = new EchoClient(sender, "devId", toBeSentPackages, receivedPackages);
+  private final String message = "hello world";
 
   @Resource
   private TemailServerProperties temailServerConfig;
@@ -91,9 +95,9 @@ public class TemailGatewayIntegrationTest {
 
 
   @BeforeClass
-  public static void beforeClass() {
+  public static void beforeClass() throws MQClientException {
     System.setProperty("temail.namesrvAddr",
-        rocketMqNameSrv.getContainerIpAddress() + ":" + 9876);
+        rocketMqNameSrv.getContainerIpAddress() + ":" + MQ_SERVER_PORT);
 
     stubFor(post(urlEqualTo("/verify"))
         .willReturn(
@@ -116,20 +120,19 @@ public class TemailGatewayIntegrationTest {
                 .withHeader(CONTENT_TYPE, APPLICATION_JSON_UTF8_VALUE)
                 .withStatus(SC_OK)
                 .withBody(GSON.toJson(Response.ok("Success")))));
+
+    createMqTopic();
   }
 
   @AfterClass
   public static void afterClass() {
     System.clearProperty("temail.namesrvAddr");
+    mqProducer.shutdown();
   }
 
   @Before
-  public void setUp() throws Exception {
+  public void setUp() {
     temailMqInf = TemailMqInfBuilder.getTemailMqInf(temailServerConfig);
-
-    mqProducer.setNamesrvAddr(rocketMqNameSrv.getContainerIpAddress() + ":" + 9876);
-    mqProducer.setInstanceName("test-producer");
-    mqProducer.start();
 
     client.start("localhost", 8099);
   }
@@ -137,8 +140,6 @@ public class TemailGatewayIntegrationTest {
   @After
   public void tearDown() {
     client.stop();
-
-    mqProducer.shutdown();
   }
 
   @Test
@@ -160,23 +161,25 @@ public class TemailGatewayIntegrationTest {
     assertThat(response.getCode()).isEqualTo(200);
     assertThat(response.getData()).isEqualTo(ackMessage);
 
-    System.out.println("*** " + aPackage);
-
-    // receive message from MQ
-    mqProducer.send(new Message(temailServerConfig.getMqTopic(), temailMqInf.getMqTag(), GSON.toJson(payload(sender)).getBytes()), 3000);
+    // heartbeat
     await().atMost(5, SECONDS).until(() -> receivedPackages.peek() != null);
     aPackage = receivedPackages.poll();
     assertThat(aPackage.getCommand()).isEqualTo(ping.getCode());
-//    assertThat(aPackage.getCommand()).isEqualTo(1000);
 
-//    response = GSON.fromJson(aPackage.getData().toStringUtf8(), Response.class);
-//    assertThat(response.getCode()).isEqualTo(200);
-//    assertThat(response.getData()).isEqualTo("hello world");
+    // receive message from MQ
+    mqProducer.send(new Message(temailServerConfig.getMqTopic(), temailMqInf.getMqTag(), GSON.toJson(payload(sender, message)).getBytes()), 3000);
+    await().atMost(5, SECONDS).until(() -> receivedPackages.peek() != null);
+    aPackage = receivedPackages.poll();
+    assertThat(aPackage.getCommand()).isEqualTo(1000);
+
+    response = GSON.fromJson(aPackage.getData().toStringUtf8(), Response.class);
+    assertThat(response.getCode()).isEqualTo(200);
+    assertThat(response.getData()).isEqualTo(message);
   }
 
   @NotNull
-  private TransferCDTPPackage payload(String recipient) {
-    Response<String> body = Response.ok("hello world");
+  private TransferCDTPPackage payload(String recipient, String message) {
+    Response<String> body = Response.ok(message);
     TransferCDTPPackage payload = new TransferCDTPPackage();
     payload.setCommand(1000);
     payload.setTo(recipient);
@@ -190,5 +193,21 @@ public class TemailGatewayIntegrationTest {
     payload.setCommand(1000);
     payload.setData(GSON.toJson(Response.ok(ackMessage)));
     return payload;
+  }
+
+  private static void createMqTopic() throws MQClientException {
+    mqProducer.setNamesrvAddr(rocketMqNameSrv.getContainerIpAddress() + ":" + MQ_SERVER_PORT);
+    mqProducer.start();
+    // ensure topic exists before consumer connects, or no message will be received
+    await().atMost(10, SECONDS).until(() -> {
+
+      try {
+        mqProducer.createTopic(mqProducer.getCreateTopicKey(), "temail-gateway", 1);
+        return true;
+      } catch (MQClientException e) {
+        e.printStackTrace();
+        return false;
+      }
+    });
   }
 }
