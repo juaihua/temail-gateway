5d72963802276a9bd2fb72babb452a1a0d456ede
姚华成 <yaohuacheng@syswin.com>
2018-10-22 09:58:13 +0800

    增加打探针需要的依赖

--- ps-common/src/main/java/com/syswin/temail/ps/common/codec/PacketEncoder.java
+++ ps-common/src/main/java/com/syswin/temail/ps/common/codec/PacketEncoder.java
@@ -7,21 +7,6 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * Created by weis on 18/8/7.
- *
- * <pre>
- * 自己定义的协议
- *  数据包格式
- * +——--------——+
- * |长度  | 数据 |
- * +——--------——+
- * 1.长度，为int类型的数据
- * 2.command , short类型
- * 3.version , short类型
- * 4.要传输的数据
- * </pre>
- */
 @Slf4j
 public class PacketEncoder extends MessageToByteEncoder<CDTPPacket> {
 
