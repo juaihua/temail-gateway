ebee87747c6c54c00f6a0d5fa31c3e3a85688171
姚华成 <yaohuacheng@syswin.com>
2018-11-01 09:57:14 +0800

    将gateway与channel的心跳日志级别从info改为debug

--- src/main/java/com/syswin/temail/gateway/channels/clients/grpc/GrpcHeartBeatManager.java
+++ src/main/java/com/syswin/temail/gateway/channels/clients/grpc/GrpcHeartBeatManager.java
@@ -49,7 +49,7 @@ class GrpcHeartBeatManager {
       executorService.scheduleWithFixedDelay(() -> {
         try {
           if (grpcClient.serverHeartBeat(gatewayServer)) {
-            log.info("heart beat success : {}-{}", gatewayServer.getIp(), gatewayServer.getProcessId());
+            log.debug("heart beat success : {}-{}", gatewayServer.getIp(), gatewayServer.getProcessId());
           } else {
             log.error("heart beat fail, try again after {} seconds .", heartBeatDelay);
           }
